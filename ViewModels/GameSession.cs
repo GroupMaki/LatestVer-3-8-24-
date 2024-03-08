b843d26ab0e6b55066fd3ae7a9ae743f12465a26
bhpr0d <156859894+bhpr0d@users.noreply.github.com>
2024-03-08 21:44:00 +0800

    Add files via upload

---
+++ ViewModels/GameSession.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Engine.Factories;
+using Engine.Models;
+
+namespace Engine.ViewModels
+{
+    public class GameSession
+    {
+        public World CurrentWorld { get; set; }
+        public Player CurrentPlayer { get; set; }
+        public Location CurrentLocation { get; set; }
+        public GameSession()
+        {
+            CurrentPlayer = new Player();
+            CurrentPlayer.Name = "Joshua";
+            CurrentPlayer.Gold = 5000000;
+            CurrentPlayer.CharacterClass = "Support";
+            CurrentPlayer.HitPoints = 20;
+            CurrentPlayer.ExperiencePoints = 0;
+            CurrentPlayer.Level = 1;
+            WorldFactory factory = new WorldFactory();
+            CurrentWorld = factory.CreateWorld();
+            CurrentLocation = CurrentWorld.LocationAt(0, 0);
+
+        }
+
+
+    }
+}
+ 
